c1e7674932adbf063759b3a6cae6895ee5925e3b
zhangpeng <1121027880@qq.com>
2023-12-09 16:01:06 +0800

    feat: 封装SvgIcon组件

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import 'virtual:svg-icons-register'
+
 import 'element-plus/theme-chalk/index.css'
 
 const app = createApp(App)
