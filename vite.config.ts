6f136602a070c04ac229454193fe1c65f1e5d287
zhangpeng <1121027880@qq.com>
2023-12-16 21:47:08 +0800

    fix: 移除stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,27 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [
-      vue(),
-      createSvgIconsPlugin({
-        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-        symbolId: 'icon-[dir]-[name]',
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
+export default defineConfig({
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    server: {
-      host: '0.0.0.0',
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-  }
+  },
 })
